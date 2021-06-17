385253b5ea3681892ffbe597ffec6492a1261918
astraszab <40484210+astraszab@users.noreply.github.com>
2021-06-17 16:35:57 +0300

    Reformat imitate by name test

--- test/test_imitate/test_imitate_by_name.py
+++ test/test_imitate/test_imitate_by_name.py
@@ -6,19 +6,26 @@ from src.selector_imitator import SelectorImitator, ImitationError
 
 
 def test_imitate_by_name():
-    user_selector = Selector(selector_type=SelectorType.BY_NAME, other_attributes={"name": "element1"})
+    user_selector = Selector(
+        selector_type=SelectorType.BY_NAME, other_attributes={"name": "element1"}
+    )
     target_node = Node(tag="h1", other_attributes={"name": "element_1"})
-    expected_result = Selector(selector_type=SelectorType.BY_NAME, other_attributes={"name": "element_1"})
+    expected_result = Selector(
+        selector_type=SelectorType.BY_NAME, other_attributes={"name": "element_1"}
+    )
     imitated_selectors = SelectorImitator(user_selector, target_node).imitate()
     assert len(imitated_selectors) == 1
     assert imitated_selectors[0] == expected_result
 
 
 def test_imitate_by_name_invalid_target():
-    user_selector = Selector(selector_type=SelectorType.BY_NAME, other_attributes={"name": "element1"})
+    user_selector = Selector(
+        selector_type=SelectorType.BY_NAME, other_attributes={"name": "element1"}
+    )
     target_node = Node(tag="h1")
     with pytest.raises(ImitationError):
         SelectorImitator(user_selector, target_node).imitate()
+
 
 def test_imitate_by_name_invalid_user_selector():
     user_selector = Selector(selector_type=SelectorType.BY_NAME)
