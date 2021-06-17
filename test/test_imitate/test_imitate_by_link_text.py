855e3cbe39ba2625284f85e9ca599a0040c32de4
astraszab <40484210+astraszab@users.noreply.github.com>
2021-06-17 16:34:42 +0300

    Add test for imitate by name

--- test/test_imitate/test_imitate_by_link_text.py
+++ test/test_imitate/test_imitate_by_link_text.py
@@ -8,7 +8,9 @@ from src.selector_imitator import SelectorImitator, ImitationError
 def test_imitate_by_link_text():
     user_selector = Selector(selector_type=SelectorType.BY_LINK_TEXT, inner_text="link")
     target_node = Node(tag="a", inner_text="link to external page")
-    expected_result = Selector(selector_type=SelectorType.BY_LINK_TEXT, inner_text="link to external page")
+    expected_result = Selector(
+        selector_type=SelectorType.BY_LINK_TEXT, inner_text="link to external page"
+    )
     imitated_selectors = SelectorImitator(user_selector, target_node).imitate()
     assert len(imitated_selectors) == 1
     assert imitated_selectors[0] == expected_result
