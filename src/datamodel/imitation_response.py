0fbc8de3b7937276c519fd7f5b262bfbee0be268
astraszab <40484210+astraszab@users.noreply.github.com>
2021-07-29 16:22:08 +0300

    Change response field names

--- src/datamodel/imitation_response.py
+++ src/datamodel/imitation_response.py
@@ -3,8 +3,8 @@ from src.selector import SelectorType
 
 
 class ImitationResponseModel(BaseModel):
-    selector_type: SelectorType = Field(..., alias="selectorType")
-    selector_value: str = Field(..., alias="selectorValue")
+    selector_type: SelectorType = Field(..., alias="type")
+    selector_value: str = Field(..., alias="value")
 
     class Config:
         allow_population_by_field_name = True
